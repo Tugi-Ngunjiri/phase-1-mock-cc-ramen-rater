fc0de46b48831fd4bd6e137ec9a9683c9800f728
Your Name <you@example.com>
2022-12-07 23:39:13 +0300

    done

---
+++ index.js
@@ -0,0 +1,23 @@
+function getGif()
+fetch;http;//localhost:3000
+fetch;gif;
+console.log(gif)
+
+
+function getRamens()
+fetch;ramens;
+console.log(ramens)
+
+
+function getRamens()
+fetch;ramens:id
+
+function getPost()
+post;ramens
+
+function getPost()
+deleteramens:id
+
+
+function getPost()
+Patch;ramens:id
